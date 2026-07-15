ed37542a43a7cdecb1d24c6050703196e6e8bb6e
Matthew <matthewspence124@gmail.com>
2026-07-15 14:24:47 +0100

    started writing flex output config file for osm2pgsql

---
+++ osm_data/style.lua
@@ -0,0 +1,5 @@
+-- define output projection to use, EPSG 3857 is the Mercator projection which is standard for tiled web maps
+local srid = 3857
+
+-- define prefix for table names
+local prefix = 'edinburgh'
