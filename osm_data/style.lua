f8687dcc8d9d9167bd6eb283940fbe0d1a5fe545
Matthew <matthewspence124@gmail.com>
2026-07-15 18:18:05 +0100

    set more config options

--- osm_data/style.lua
+++ osm_data/style.lua
@@ -3,3 +3,12 @@ local srid = 3857
 
 -- define prefix for table names
 local prefix = 'edinburgh'
+
+-- don't use OSM coastline data
+local keep_coastlines = false
+
+-- write multipolygons into database as multipolygons
+local multi_geometry = true
+
+-- initialise storage for defining SQL tables
+local tables = {}
