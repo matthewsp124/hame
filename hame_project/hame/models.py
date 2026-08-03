75051b5e01575e04235585f9ddbc2d2acf440940
Matthew <matthewspence124@gmail.com>
2026-08-03 16:02:02 +0100

    restructured database, created database seed script

--- hame_project/hame/models.py
+++ hame_project/hame/models.py
@@ -1,49 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class LocationCategoryKey(models.Model):
+    osm_key = models.CharField(max_length = 64, unique = True)
+
+    def __str__(self):
+        return self.osm_key
+
 class LocationCategory(models.Model):
-    name = models.CharField(max_length = 128)
-    osm_key = models.CharField(max_length = 64, null = True, blank = True)
+    key = models.ForeignKey(LocationCategoryKey, on_delete = models.SET_NULL, null = True, blank = False)
     osm_value = models.CharField(max_length = 64, null = True, blank = True)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields = ['osm_key', 'osm_value'], name = 'unique_osm_key_value')
+            models.UniqueConstraint(fields = ['key', 'osm_value'], name = 'unique_osm_key_value')
         ]
 
     def __str__(self):
-        return self.name
-
-def get_default_category():
-    category, created = LocationCategory.objects.get_or_create(
-        name = 'Uncategorised',
-        defaults = {'osm_key': None, 'osm_value': None}
-    )
-    return category.pk
+        return f"{self.key.osm_key}={self.osm_value}" 
 
 class Location(models.Model):
-    source_choices = [('osm', 'OpenStreetMap'), ('user', 'User-added')]
-    name = models.CharField(max_length = 128)
-    address = models.CharField(max_length = 256)
+    name = models.CharField(max_length = 128, null = True)
+    address = models.CharField(max_length = 256, blank = True)
     lat = models.FloatField()
     lng = models.FloatField()
-    category = models.ForeignKey(LocationCategory, on_delete = models.SET_DEFAULT, default = get_default_category, null = False) # TODO: get default category id
-    source = models.CharField(max_length = 16, choices = source_choices, default = 'osm')
-    osm_type = models.CharField(max_length = 1, null = True, blank = True)
+    categories = models.ManyToManyField(LocationCategory, blank = False)
+    osm_type = models.CharField(max_length = 1, null = True, blank = True)  # currently treating all locations as nodes, but node and way IDs can overlap 
     osm_id = models.BigIntegerField(null = True, blank = True)
     tags = models.JSONField(default = dict, blank = True)
-    created_by = models.ForeignKey(User, on_delete = models.PROTECT, null = True)  # protect because deletion of users should be rare and handled by admin in case of vandalism
-    created_at = models.DateTimeField(auto_now_add = True)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields = ['osm_type', 'osm_id'], name = 'unique_osm_type_id', 
-                                    condition = models.Q(source = 'osm'))
+            models.UniqueConstraint(fields = ['osm_type', 'osm_id'], name = 'unique_osm_type_id')
         ]
-        
 
     def __str__(self):
-        return self.name
+        return f"{self.osm_id} - {self.name or self.address} ({self.lat}, {self.lng})"
 
 class UserEntry(models.Model):
     location = models.ForeignKey(Location, on_delete = models.CASCADE, related_name = 'user_entries')
