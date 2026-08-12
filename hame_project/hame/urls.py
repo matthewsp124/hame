7a6875c00acbad3eec4dbb8dc648ac5f1161e538
Matthew <matthewspence124@gmail.com>
2026-08-12 12:36:05 +0100

    implemented logout button

--- hame_project/hame/urls.py
+++ hame_project/hame/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('resources/', views.resources, name='resources'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='hame/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.user_logout, name='logout'),
     path('profile/', views.profile, name='profile'),
     path('api/locations/', views.locations_geojson, name='locations_geojson')
 ]
