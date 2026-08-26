ed862b14718c7092d120b55e0ffc139bdbab5de7
Matthew <matthewspence124@gmail.com>
2026-08-26 11:38:35 +0100

    db change

--- hame_project/hame_project/settings.py
+++ hame_project/hame_project/settings.py
@@ -83,23 +83,15 @@ WSGI_APPLICATION = 'hame_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='REDACTED',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'edinburgh_map',
+        'USER': 'hame_user',
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
-
-# local database for testing:
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'edinburgh_map',
-#         'USER': 'hame_user',
-#         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 
 
 # Password validation
