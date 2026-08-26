453e9ec7effa5a5269e8dcbf27f46941a6d0063c
Matthew <matthewspence124@gmail.com>
2026-08-26 12:05:33 +0100

    updated staticfiles settings for django 6

--- hame_project/hame_project/settings.py
+++ hame_project/hame_project/settings.py
@@ -137,6 +137,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
